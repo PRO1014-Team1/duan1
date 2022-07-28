015877a397dba7e9a4d0f2244302807bc754af69
Vu2910 <ximaxi2022@gmail.com>
2022-07-28 10:51:34 +0700

    nút delete giỏ hàng

--- views/user/cart.php
+++ views/user/cart.php
@@ -7,6 +7,16 @@ if (isset($_POST['submit'])) {
     }
     redirect('checkout');
 }
+if (isset($_GET['action'])){
+    $id=$_GET['id'];
+    switch ($_GET['action']) {
+        case 'delete';
+        unset($_SESSION['cart'][$id]);
+        break;
+    }
+    redirect('cart');
+}
+
 
 ?>
 <div class="container">
@@ -28,6 +38,7 @@ if (isset($_POST['submit'])) {
                                     <th>Tên sản phẩm</th>
                                     <th>Giá</th>
                                     <th>Số lượng</th>
+                                    <th></th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -60,6 +71,11 @@ if (isset($_POST['submit'])) {
                                                     <input class="quantity__idicator" type="number" name="quantity[]" min="<?= $min ?>" max="<?= $max ?>" step="1" value="<?= $item['quantity'] ?>">
                                                 </div>
                                             </td>
+                                            <td> 
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+  <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"onclick="location='?option=cart&action=delete&id=<?=$item['id']?>';">
+</svg>
+                                    </td>
                                         </tr>
                                     <?php endforeach; ?>
                                 <?php endif; ?>
